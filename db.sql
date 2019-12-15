9c76b4622ddfa50689277ff97cc3509152de7e39
Hoàng <58172756+hoang01bk@users.noreply.github.com>
2019-12-15 21:54:08 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,10 @@
-﻿﻿CREATE TABLE users (
+CREATE TABLE users (
 	id serial PRIMARY KEY,
 	name VARCHAR(100),
 	email text UNIQUE NOT NULL,
 	phone VARCHAR(100) NOT NULL,
 	joined TIMESTAMP NOT NULL
 );
-drop table users;
 ALTER TABLE "users" OWNER TO myuser;
 ALTER TABLE "users" OWNER TO postgres;
 
@@ -25,78 +24,43 @@ CREATE TABLE products (
 	supplier VARCHAR(100),
 	check(price >=0)	
 );
---bang khac khong tao rang buoc
-CREATE TABLE products (
-	id serial PRIMARY KEY,
-	name VARCHAR(100),
-	price int,
-	img VARCHAR(100),
-	description VARCHAR(1000)
-);
 
-ALTER TABLE "products" OWNER TO myuser;
+ALTER TABLE "products" OWNER TO postgres;
 
 INSERT INTO products(name, price, img, description, category, supplier)
-VALUES('digital camera',300, 'themes/images/products/1.jpg','day chi la mo ta demo thoi',1,'USA')
-
+VALUES('Áo bóng rổ',100,'themes/images/products/anh1.jpg',NULL,1,'USA')
+;
 INSERT INTO products(name, price, img, description, category, supplier)
-VALUES('Red digital camera',300, 'themes/images/products/2.jpg','day chi la mo ta demo thoi',1,'SONY')
-
+VALUES('Quần jeans',120, 'themes/images/products/anh2.jpg','Ôm dáng',2,'Levis')
+;
 INSERT INTO products(name, price, img, description, category, supplier)
-VALUES('Big digital camera',350, 'themes/images/products/4.jpg','hahahahahahaha',2,'Made in VietNam')
-
+VALUES('Quần thể thao',50, 'themes/images/products/anh3.jpg','Chất liệu thấm hút',2,'Made in VietNam')
+;
 INSERT INTO products(name, price, img, description, category, supplier)
-VALUES('SD card',150, 'themes/images/products/6.jpg','helehehehehe',2,'VDN')
-
+VALUES('Áo ngắn tay',45, 'themes/images/products/anh4.jpg','vải cotton',1,'Made in VietNam')
+;
 INSERT INTO products(name, price, img, description, category, supplier)
-VALUES('USB',220, 'themes/images/products/7.jpg','ppppppppppp',1,'China')
-
+VALUES('Áo thể thao',220, 'themes/images/products/anh5.jpg','Chính hãng',1,'Adidas')
+;
 INSERT INTO products(name, price, img, description, category, supplier)
-VALUES('boot',170, 'themes/images/products/b1.jpg','lalalalalalal',2,'Adidas')
-
+VALUES('Quần jogger',70, 'themes/images/products/anh6.jpg','Thời trang',2,'Adidas')
+;
 INSERT INTO products(name, price, img, description, category, supplier)
-VALUES('panasonic camera',750, 'themes/images/products/13.jpg','Made in Japan || Design by Panasonic',1,'Panasonic')
-
+VALUES('Quần tây',75, 'themes/images/products/anh7.jpg','Kiểu dáng mới',2,'Uniqlo')
+;
 INSERT INTO products(name, price, img, description, category, supplier)
-VALUES('Ipad',840, 'themes/images/products/kindle.png','vnvnvnvnvnvnnvvn',2,'Kindle')
-
-DROP TABLE products 
-
---UPDATE products
---SET "img" = 'themes/images/products/kindle.png'
---WHERE "name"='Ipad'
---UPDATE products
---SET "img" = 'themes/images/products/13.jpg'
---WHERE "name"='panasonic camera'
-
+VALUES('Áo hoodie',84, 'themes/images/products/anh8.jpg','100% cotton',1,'USA')
+;
 CREATE TABLE carousels (
 	id serial PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	img VARCHAR(100) NOT NULL
 );
-drop table carousels;
-ALTER TABLE "carousels" OWNER TO myuser;
-
+ALTER TABLE "carousels" OWNER TO postgres;
 INSERT INTO carousels(name,img)
-VALUES('anh 1','themes/images/carousel/1.png')
-
-INSERT INTO carousels(name, img)
-VALUES('carousels thu 2','themes/images/carousel/2.png')
-
-INSERT INTO carousels(name, img)
-VALUES('Cai 3','themes/images/carousel/3.png')
-
-INSERT INTO carousels(name, img)
-VALUES('Anh thu 4','themes/images/carousel/4.png')
-
-INSERT INTO carousels(name, img)
-VALUES('Cai thu 6','themes/images/carousel/5.png')
-
-INSERT INTO carousels(name, img)
-VALUES('Cai Anh thu 7 kakkaak','themes/images/carousel/6.png')
-
-INSERT INTO carousels(name, img)
-VALUES('Anh 3','assets/img/slider/3.jpg')
+VALUES('anh 1','themes/images/carousel/carousel1.jpg');
+INSERT INTO carousels(name,img)
+VALUES('anh 2','themes/images/carousel/carousel2.jpg');
 
 CREATE TABLE cartsessions (
 	id serial PRIMARY KEY,
@@ -109,19 +73,14 @@ CREATE TABLE cartsessions (
 	quantity int
 );
 
-ALTER TABLE "cartsessions" OWNER TO myuser;
+ALTER TABLE "cartsessions" OWNER TO postgres;
 
 CREATE TABLE sessions (
 	id serial PRIMARY KEY,
 	sessionId VARCHAR(100)
 );
 
-ALTER TABLE "sessions" OWNER TO myuser;
-
-DROP TABLE sessions;
-
-DELETE from cartsessions
-where description LIKE '%i%'
+ALTER TABLE "sessions" OWNER TO postgres;
 
 CREATE TABLE orders (
 	id serial PRIMARY KEY,
@@ -135,22 +94,14 @@ CREATE TABLE orders (
 	totalprice INT,
 	status INT DEFAULT 0
 );
-ALTER TABLE "orders" OWNER TO myuser;
-drop table orders;
-
-DELETE from orders
-where email LIKE '%m%'
-DELETE from orders
-where totalprice = 3210
+ALTER TABLE "orders" OWNER TO postgres;
 
 CREATE TABLE admin (
 	id serial PRIMARY KEY,
 	hash varchar(100) NOT NULL,
 	email text UNIQUE NOT NULL
 );
-ALTER TABLE "admin" OWNER TO myuser;
-INSERT INTO admin(hash, email)
-VALUES('"$2a$10$awhsUxT3SU.b7wBLT5DKQuh3o2LMti/g/rLNFeG2vC1nomsOxjXpW"','nguyenvd@gmail.com')
+ALTER TABLE "admin" OWNER TO postgres;
 
 CREATE TABLE orderdetail (
 	id serial PRIMARY KEY,
@@ -163,5 +114,7 @@ CREATE TABLE orderdetail (
 	quantity int
 );
 
-ALTER TABLE "orderdetail" OWNER TO myuser;
-drop table orderdetail;
+ALTER TABLE "orderdetail" OWNER TO postgres;
+alter table orderdetail add constraint fk_productID foreign key (productID) references products(id);
+alter table orderdetail add constraint fk_orderID foreign key (orderID) references orders(id);
+alter table cartsessions add constraint fk_productID foreign key (productID) references products(id);
